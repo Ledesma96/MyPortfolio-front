f8e98dcbfb4bb569713c87a1a11458571718016a
Gabriel <gabriel.m.ledesma96@gmail.com>
2024-05-14 20:34:41 -0300

    first commit

--- src/app/contact/components/Social.jsx
+++ src/app/contact/components/Social.jsx
@@ -8,31 +8,31 @@ const Social = () => {
             <section className='container-contact_div_div_section'>
                 <div className='container-contact_div_div_section_div'>
                     <Link className='link' href='https://github.com/Ledesma96' target='blank'>
-                        <img className='container-contact_div_div_section_div_img' src='/ejry4wslmps91.png'></img>
+                        <img className='container-contact_div_div_section_div_img' src='/ejry4wslmps91.png' alt='github logo'></img>
                         <p className='container-contact_div_div_section_div_p'>GitHub</p>
                     </Link>
                 </div>
                 <div className='container-contact_div_div_section_div'>
                     <Link className='link'  href='https://www.linkedin.com/in/gabriel-matias-ledesma/' target='blank'>
-                        <img  className='container-contact_div_div_section_div_img' src='/linkedin_pixel_logo_icon_181925.png'></img>
+                        <img  className='container-contact_div_div_section_div_img' src='/linkedin_pixel_logo_icon_181925.png' alt='linkedin logo'></img>
                         <p className='container-contact_div_div_section_div_p'>Linkedin</p>
                     </Link>
                 </div>
                 <div className='container-contact_div_div_section_div'>
-                    <Link className='link'  href='https://api.whatsapp.com/send?phone=5491164323570' target='blank'>
-                        <img className='container-contact_div_div_section_div_img' src='/whatsapp_pixel_logo_icon_181923.png'></img>
+                    <Link className='link'  href='https://api.whatsapp.com/send?phone=5491164323570' target='_blank'>
+                        <img className='container-contact_div_div_section_div_img' src='/whatsapp_pixel_logo_icon_181923.png' alt='whatsapp logo'></img>
                         <p className='container-contact_div_div_section_div_p'>+54 911 6432-3570</p>
                     </Link>
                 </div>
                 <div className='container-contact_div_div_section_div'>
                     <Link className='link' href='tel:+5491164323570'>
-                        <img className='container-contact_div_div_section_div_img' src='/pngtree-pixel-art-call-center-icon-design-vector-png-image_8532647.png'></img>
+                        <img className='container-contact_div_div_section_div_img' src='/pngtree-pixel-art-call-center-icon-design-vector-png-image_8532647.png' alt='telephone logo'></img>
                         <p className='container-contact_div_div_section_div_p'>+54 911 6432-3570</p>
                     </Link>
                 </div>
                 <div className='container-contact_div_div_section_div'>
                     <Link className='link'  href="mailto:gabriel.m.ledesma96@gmail.com">
-                        <img className='container-contact_div_div_section_div_img' src='/mail-letter-pixel-free-vector.png'></img>
+                        <img className='container-contact_div_div_section_div_img' src='/mail-letter-pixel-free-vector.png' alt='email logo'></img>
                         <p className='container-contact_div_div_section_div_p' >gabriel.m.ledesma96@gmail.com</p>
                     </Link>
                 </div>
